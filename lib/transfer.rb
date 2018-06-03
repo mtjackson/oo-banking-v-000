0e7b231f07c627cf21d58d6acfe39f952d07703c
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 01:00:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,8 +34,6 @@ class Transfer
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
       self.status = "reversed"
-    elsif self.status != "complete"
-      nil
     end
   end
 end
